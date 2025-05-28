a7f2fe0e6df5a36586c5fc5175528f1f00053492
DanielBarros854 <danielbarros854@yahoo.com>
2025-05-27 23:07:27 -0300

    feat: create sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import { client } from '../graphql/client'
 import Navbar from '../components/layout/navbar'
+import Sidebar from '../components/layout/sidebar'
 import { Box } from '@mui/material'
 import PrivateRoute from '../components/auth/PrivateRoute'
 import { useRouter } from 'next/router'
@@ -14,14 +15,22 @@ export default function App({ Component, pageProps }: AppProps) {
         <ApolloProvider client={client}>
             <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
                 {!isLoginPage && <Navbar />}
-                <Box component="main" sx={{ flexGrow: 1, mt: !isLoginPage ? 8 : 0, p: 3 }}>
-                    {isLoginPage ? (
-                        <Component {...pageProps} />
-                    ) : (
-                        <PrivateRoute>
+                <Box sx={{ display: 'flex', flexGrow: 1 }}>
+                    {!isLoginPage && <Sidebar />}
+                    <Box component="main" sx={{ 
+                        flexGrow: 1, 
+                        mt: !isLoginPage ? 8 : 0, 
+                        p: 3,
+                        transition: 'margin-left 1s ease-in-out',
+                    }}>
+                        {isLoginPage ? (
                             <Component {...pageProps} />
-                        </PrivateRoute>
-                    )}
+                        ) : (
+                            <PrivateRoute>
+                                <Component {...pageProps} />
+                            </PrivateRoute>
+                        )}
+                    </Box>
                 </Box>
             </Box>
         </ApolloProvider>
